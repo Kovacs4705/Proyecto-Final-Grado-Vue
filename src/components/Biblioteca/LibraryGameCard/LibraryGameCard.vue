cf11e20a72fb5ed2915821f173a69aa23f179f38
Kovacs4705 <kovacs123david@gmail.com>
2025-05-31 21:36:29 +0200

    Biblioteca completa

--- src/components/Biblioteca/LibraryGameCard/LibraryGameCard.vue
+++ src/components/Biblioteca/LibraryGameCard/LibraryGameCard.vue
@@ -1,7 +1,7 @@
 <!-- src/components/LibraryGameCard.vue -->
 <template>
   <div
-    class="card library-game-card"
+    class="card"
     data-aos="fade-up"
     data-aos-duration="1200"
     :data-aos-delay="delay"
@@ -63,19 +63,19 @@ export default {
   --bs-card-bg: transparent;
 }
 
-.card .library-game-card{
+.card .library-game-image{
     transform: scale(1.2);
 }
 
 /* Contenedor principal de la tarjeta */
-.library-game-card {
+/* .library-game-card {
   width: 100%;
   border: none;
   border-radius: 10px;
   overflow: hidden;
   transition: transform 0.2s ease;
   box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.4);
-}
+} */
 
 /* Efecto hover para elevar ligeramente la tarjeta */
 .library-game-card:hover {
@@ -83,31 +83,18 @@ export default {
 }
 
 /* Imagen de portada del juego */
-.library-game-image {
+/* .library-game-image {
   display: block;
   width: 100%;
-  height: 470px;             /* Altura fija: ajusta según prefieras (250–300px es un buen rango) */
-  object-fit: cover;         /* Para que la imagen mantenga proporción y recorte si es necesario */
+  height: 470px;             
+  object-fit: cover;         
   border-top-left-radius: 12px;
   border-top-right-radius: 12px;
-    /* Color de respaldo si tarda en cargar la imagen */
-}
+   
+} */
 
 /* Cuerpo de la tarjeta que contiene el título */
-.card-body {
-  background: linear-gradient(
-    180deg,
-    rgba(36, 57, 83, 1) 0%,
-    rgba(10, 10, 10, 0.95) 67%,
-    rgba(23, 23, 23, 0.91) 100%
-  );
-  padding: 0.75rem 0.5rem;
-  text-align: left;
-  height: 70px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+
 
 /* Nombre del juego */
 .game-name {
