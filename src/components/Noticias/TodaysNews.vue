8288d36e0081b8a4122f51f684892ba2fc096ee2
Kovacs4705 <kovacs123david@gmail.com>
2025-06-01 19:29:02 +0200

    Noticias esta falta corregir errores

---
+++ src/components/Noticias/TodaysNews.vue
@@ -0,0 +1,258 @@
+<template>
+  <div class="todays-news">
+    <!-- Título principal con AOS -->
+    <h2
+      id="destacado"
+      class="text-center mt-3"
+      data-aos="fade-right"
+      data-aos-duration="2000"
+    >
+      Noticias de Hoy
+    </h2>
+
+    <!-- Bloque de noticias destacadas -->
+    <div
+      class="row align-items-center mt-4 featured-news-item"
+      v-for="(item, index) in featuredNews"
+      :key="index"
+      data-aos="fade-right"
+      data-aos-duration="2000"
+    >
+      <div class="col-md-4">
+        <img
+          :src="item.image"
+          alt="Imagen Destacado"
+          class="img-fluid rounded featured-img"
+        />
+      </div>
+      <div class="col-md-8 text-start textoNoticia">
+        <h3 class="news-title">{{ item.title }}</h3>
+        <p class="news-description">{{ item.description }}</p>
+        <button class="btn btn-success rounded-pill px-4">Leer más</button>
+      </div>
+    </div>
+
+    <!-- Separador -->
+    <hr class="separator my-5" />
+
+    <!-- Bloque de noticias secundarias -->
+    <div
+      class="row small-news-grid"
+      data-aos="fade-right"
+      data-aos-duration="2000"
+    >
+      <div
+        class="col-md-4 mb-4 small-news-item"
+        v-for="(item, idx) in smallNews"
+        :key="idx"
+      >
+        <img
+          :src="item.image"
+          alt="Imagen Noticia"
+          class="img-fluid rounded small-news-img"
+        />
+        <h4 class="small-news-title">{{ item.title }}</h4>
+        <p class="small-news-description">{{ item.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodaysNews",
+  data() {
+    return {
+      featuredNews: [
+        {
+          image: "/images/NoticiaDestacada.png",
+          title: "UNREAL TOURNAMENT",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, a aliquid corrupti illo distinctio minus, voluptates cum nobis ex enim deleniti reiciendis."
+        },
+        {
+          image: "/images/rocket.png",
+          title: "ROCKET LAUNCH EVENT",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, a aliquid corrupti illo distinctio minus, voluptates cum nobis ex enim deleniti reiciendis."
+        },
+        {
+          image: "/images/fornitenoticia.png",
+          title: "FORTNITE UPDATES",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, a aliquid corrupti illo distinctio minus, voluptates cum nobis ex enim deleniti reiciendis."
+        }
+      ],
+      smallNews: [
+        {
+          image: "/images/fornitenoticia.png",
+          title: "Noticia 1",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, a aliquid corrupti illo distinctio minus."
+        },
+        {
+          image: "/images/rocket.png",
+          title: "Noticia 2",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, a aliquid corrupti illo distinctio minus."
+        },
+        {
+          image: "/images/fornitenoticia.png",
+          title: "Noticia 3",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, a aliquid corrupti illo distinctio minus."
+        },
+        {
+          image: "/images/rocket.png",
+          title: "Noticia 4",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, a aliquid corrupti illo distinctio minus."
+        },
+        {
+          image: "/images/fornitenoticia.png",
+          title: "Noticia 5",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, a aliquid corrupti illo distinctio minus."
+        },
+        {
+          image: "/images/rocket.png",
+          title: "Noticia 6",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, a aliquid corrupti illo distinctio minus."
+        }
+      ]
+    };
+  },
+  mounted() {
+    // Inicializar AOS al montar el componente
+    if (typeof AOS !== "undefined") {
+      AOS.init();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todays-news {
+  margin-top: 2rem;
+  margin-bottom: 4rem;
+  /* El contenedor principal no necesita más padding porque el wrapper global ya lo aplica */
+}
+
+/* --------------------------------------------------------------------------
+   TÍTULO PRINCIPAL
+   -------------------------------------------------------------------------- */
+#destacado {
+  position: relative;
+  margin-bottom: 3rem;
+  font-family: "Oswald", sans-serif;
+  font-size: 2rem;
+  color: #ffffff;
+}
+#destacado::after {
+  content: "";
+  position: absolute;
+  bottom: -8px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 6%;
+  height: 3px;
+  background-color: #39ff14;
+}
+
+/* --------------------------------------------------------------------------
+   NOTICIAS DESTACADAS
+   -------------------------------------------------------------------------- */
+.featured-news-item {
+  margin-bottom: 3rem;
+}
+
+.featured-img {
+  width: 100%;
+  object-fit: cover;
+  transition: transform 0.3s ease;
+}
+.featured-img:hover {
+  transform: scale(1.02);
+}
+
+.textoNoticia .news-title {
+  font-size: 1.75rem;
+  margin-bottom: 0.75rem;
+  color: #ffffff;
+}
+
+.textoNoticia .news-description {
+  font-size: 1rem;
+  line-height: 1.5;
+  color: #dddddd;
+  margin-bottom: 1rem;
+}
+
+/* --------------------------------------------------------------------------
+   SEPARADOR
+   -------------------------------------------------------------------------- */
+.separator {
+  border: none;
+  height: 1px;
+  background-color: rgba(255, 255, 255, 0.3);
+  width: 80%;
+  margin-left: 10%;
+  margin-right: 10%;
+}
+
+/* --------------------------------------------------------------------------
+   NOTICIAS SECUNDARIAS (GRID)
+   -------------------------------------------------------------------------- */
+.small-news-grid {
+  margin-top: 2rem;
+}
+
+.small-news-item {
+  text-align: left;
+}
+
+.small-news-img {
+  width: 80%;
+  object-fit: cover;
+  margin-bottom: 0.5rem;
+}
+
+.small-news-title {
+  font-size: 1.25rem;
+  color: #ffffff;
+  margin-bottom: 0.5rem;
+}
+
+.small-news-description {
+  font-size: 0.95rem;
+  color: #cccccc;
+  line-height: 1.4;
+}
+
+/* --------------------------------------------------------------------------
+   AOS (se asegura que no interfiera con estilos existentes)
+   -------------------------------------------------------------------------- */
+[data-aos] {
+  opacity: 0;
+  transition-property: opacity, transform;
+}
+
+[data-aos].aos-animate {
+  opacity: 1;
+}
+
+/* --------------------------------------------------------------------------
+   UTILITARIOS
+   -------------------------------------------------------------------------- */
+.mt-3 {
+  margin-top: 1rem !important;
+}
+.my-5 {
+  margin-top: 3rem !important;
+  margin-bottom: 3rem !important;
+}
+.mb-4 {
+  margin-bottom: 1.5rem !important;
+}
+</style>
--- src/views/NoticiasView.vue
+++ src/views/NoticiasView.vue
@@ -1,15 +1,138 @@
 <!-- src/views/NoticiasView.vue -->
 <template>
-    <div class="noticias-view">
-        <!-- Aquí irá tu NewsHero, NewsArchive, etc. -->
-        <h1>Noticias</h1>
-    </div>
+  <div class="noticias-view container mt-4">
+    <!-- Sección Noticias de Hoy -->
+    <section class="section-todays-news">
+      <TodaysNews />
+    </section>
+
+    <!-- Separador -->
+    <hr class="mt-5 mb-5 separator" />
+
+    <!-- Sección Noticias Generales -->
+    <section class="section-general-news">
+      <h2
+        class="general-title text-center mb-4"
+        data-aos="fade-right"
+        data-aos-duration="2000"
+      >
+        Últimas Noticias
+      </h2>
+      <div class="row">
+        <div
+          class="col-md-4"
+          v-for="(item, index) in generalNewsList"
+          :key="index"
+          data-aos="fade-right"
+          data-aos-duration="2000"
+        >
+          <GeneralNewsCard
+            :title="item.title"
+            :description="item.description"
+            :image="item.image"
+          />
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup>
-// Aquí importas tus componentes y lógica
+import { onMounted, ref } from "vue";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+import TodaysNews from "../components/Noticias/TodaysNews.vue";
+import GeneralNewsCard from "../components/Noticias/GeneralNews.vue";
+
+const generalNewsList = ref([
+  {
+    image: "/images/fornitenoticia.png",
+    title: "Noticia General 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, corrupti illo distinctio minus."
+  },
+  {
+    image: "/images/rocket.png",
+    title: "Noticia General 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, corrupti illo distinctio minus."
+  },
+  {
+    image: "/images/fornitenoticia.png",
+    title: "Noticia General 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, corrupti illo distinctio minus."
+  },
+  {
+    image: "/images/rocket.png",
+    title: "Noticia General 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, corrupti illo distinctio minus."
+  },
+  {
+    image: "/images/fornitenoticia.png",
+    title: "Noticia General 5",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, corrupti illo distinctio minus."
+  },
+  {
+    image: "/images/rocket.png",
+    title: "Noticia General 6",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, corrupti illo distinctio minus."
+  }
+]);
+
+onMounted(() => {
+  AOS.init();
+});
 </script>
 
 <style scoped>
-/* Tus estilos locales */
+.noticias-view {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding-bottom: 4rem;
+}
+
+/* Separador */
+.separator {
+  border: none;
+  height: 1px;
+  background-color: rgba(255, 255, 255, 0.2);
+}
+
+/* Título sección Noticias Generales */
+.general-title {
+  font-family: "Oswald", sans-serif;
+  font-size: 2rem;
+  color: #ffffff;
+  position: relative;
+}
+.general-title::after {
+  content: "";
+  position: absolute;
+  bottom: -8px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 6%;
+  height: 3px;
+  background-color: #39ff14;
+}
+
+/* Ajustes de responsive para cards */
+.section-general-news .col-md-4 {
+  margin-bottom: 2rem;
+}
+
+/* Margen superior para Noticias de Hoy */
+.section-todays-news {
+  margin-top: 2rem;
+}
+
+/* Colocar espacio debajo de cada tarjeta */
+.general-news-card {
+  background-color: transparent;
+}
 </style>
